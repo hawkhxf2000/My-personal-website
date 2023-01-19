69ce0dc4dc4cfa4d1d3168f25b36abe2e1934c59
XIAOFENG HUANG <hawkhxf2000@gmail.com>
2023-01-18 16:16:49 -0800

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -18,7 +18,7 @@ export default async function(req:NextApiRequest){
         requireTLS: true,
         auth: {
             user: "hawkhxf2000@gmail.com",
-            pass: "gurhfpyyiqyhhkly"
+            pass: 
         },
         logger: true
     });
